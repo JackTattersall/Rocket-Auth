c7d1ae3918a0486fb1db25fb66a7e0eb4a70ee0a
JackTattersall <jack.tattersall@opencastsoftware.com>
2017-07-04 12:04:51 +0100

    added handlebars and basic template

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,14 @@
 #![feature(custom_attribute)]
 #![feature(const_fn)]
 #![plugin(rocket_codegen)]
+
+#[macro_use] extern crate serde_derive;
+
 pub mod router;
+
 extern crate rocket;
-
-
+extern crate serde_json;
+extern crate serde;
 
 #[cfg(test)]
 mod tests {
--- src/router.rs
+++ src/router.rs
@@ -1,16 +1,27 @@
+extern crate rocket_contrib;
 extern crate rocket;
+extern crate serde_json;
+
+use self::rocket_contrib::Template;
+
+#[derive(Serialize, Deserialize)]
+pub struct TemplateContext {
+    name: String,
+}
 
 #[get("/")]
-pub const fn index() -> &'static str {
-    "Hello, world!"
+pub fn index() -> Template {
+    let context = TemplateContext{ name: "Jack".to_string() };
+
+    Template::render("index", &context)
 }
 
 #[get("/register")]
-pub const fn register() -> &'static str {
+pub fn register() -> &'static str {
     "Registration page"
 }
 
 #[get("/login")]
-pub const fn login() -> &'static str {
+pub fn login() -> &'static str {
     "Login page"
 }
